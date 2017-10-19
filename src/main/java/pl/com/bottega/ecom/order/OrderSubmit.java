5645f63bdbe6614c41632f9b0185b5e78c528248
Maciej Zubala <maciej.zubala@gmail.com>
2017-10-19 15:36:57 +0200

    order module api

---
+++ src/main/java/pl/com/bottega/ecom/order/OrderSubmit.java
@@ -0,0 +1,7 @@
+package pl.com.bottega.ecom.order;
+
+public interface OrderSubmit {
+
+    void submit(String cartId, String token);
+
+}
---
+++ src/main/java/pl/com/bottega/ecom/order/PaymentService.java
@@ -0,0 +1,7 @@
+package pl.com.bottega.ecom.order;
+
+public interface PaymentService {
+
+    void captureCreditCard(String token);
+
+}
